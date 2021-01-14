1d0d41ee87e93a91314073978f224ac08b87df5e
Omar G <kokolenne@yahoo.com>
2021-01-14 17:25:19 -0600

    Express Session Part 2

--- app.js
+++ app.js
@@ -51,54 +51,34 @@ app.use(
   })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      //if authHeader is null, user hasn't put username and password
       const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic"); //let client know server is requesting authentication
-      err.status = 401; //set the status code
+      err.status = 401; 
       return next(err);
-    }
-    //If there's an authorization header, parse the authorization header. base64 to decode.
-    //Takes the authorization header
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === "admin" && pass === "password") {
-      req.session.user = "admin";
-      return next(); // authorized
-    } else {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-  } else {
-    //if there is a sign cookie dot user value in the incoming request
-    if (req.session.user === "admin") {
-      //grant access by passing the client on to the next middleware function using next()
+  }else {
+    if(req.session.user === 'authenticated') {
       return next();
-    } else {
-      const err = new Error("You are not authenticated!");
+    } else{
+      const err= new Error('You are not autenticated!');
       err.status = 401;
       return next(err);
     }
   }
 }
+    
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
 //Can put it here if you don't mind user accessing the static data without authenticating, You can put it below the above line (here)
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionsRouter);
 app.use("/partners", partnerRouter);
